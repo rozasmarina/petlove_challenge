99f1c05b85d44438694b7bb885eaec96ccf9e7a3
Marina Rozas <rozasmarina@gmail.com>
2020-10-05 12:22:50 -0300

    seed first draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Creating owners"
+sleep(1)
+
+counter = 0
+pet_name = ['Pé de Pano', 'Rex', 'Ajudante do Papai Noel', 'Rex', 'Flora', 'Dino', 'Lassie']
+monthly_cost = ['199.99', '99.99', '99.99', '103.99', '103.99', '177.99', '407.99']
+type = %w[Cavalo Cachorro Cachorro Papagaio Lhama Iguana Ornitorrinco]
+owner_name = ['Johnny Cash', 'Sid Vicious', 'Axl Rose', 'Joey Ramone', 'Bruce Dickinson', 'Kurt Cobain', 'Elvis Presley']
+document = '555555555'
+birthday = ['26/02/1932', '10/05/1957', '06/02/1962', '19/05/1951', '07/08/1958', '20/02/1967', '17/08/2008']
+
+7.times do
+  Pet.create!(
+    name: pet_name[counter], 
+    monthly_cost: monthly_cost[counter], 
+    type: type[counter],
+    owner: Owner.create (
+      name: owner_name[counter]
+      document: document,
+      birthday: birthday[counter]
+    )
+  )
+  counter += 1
+end
