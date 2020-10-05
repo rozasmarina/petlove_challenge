d363302da047f328b14edea7cd62e353cc7e23ca
Marina Rozas <rozasmarina@gmail.com>
2020-10-05 13:50:43 -0300

    save WIP on classes validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,7 @@
 class Owner < ApplicationRecord
   has_many :pets
 
-  def can_have_swallow?
-    
-  end
+  # def can_have_swallow?
+  #   DateTime.current.to_date - self.birthday > 18
+  # end
 end
