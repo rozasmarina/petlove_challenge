9b15541cb27059cc2f07da09efc930908eec0df6
Marina Rozas <rozasmarina@gmail.com>
2020-10-05 12:44:26 -0300

    tables ok

--- db/migrate/20201005145706_create_pets.rb
+++ db/migrate/20201005145706_create_pets.rb
@@ -1,11 +1,10 @@
 class CreatePets < ActiveRecord::Migration[6.0]
   def change
     create_table :pets do |t|
-      t.string :name, null:false
+      t.string :name, null: false
       t.float :monthly_cost
-      t.string :type, null:false
-      t.references :owner_id, null: false, foreign_key: true,
-
+      t.string :type, null: false
+      t.references :owner, null: false, foreign_key: true
       t.timestamps
     end
   end
