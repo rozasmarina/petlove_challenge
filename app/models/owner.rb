dce5039c953af51d2158bec491b7d5869b188ceb
Marina Rozas <rozasmarina@gmail.com>
2020-10-06 10:26:25 -0300

    owner model functions first draft

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,17 @@
 class Owner < ApplicationRecord
   has_many :pets
+  validates :name, :document, :birthday, presence: true
 
-  # def can_have_swallow?
-  #   DateTime.current.to_date - self.birthday > 18
-  # end
+  def can_have_swallow?
+    Date.today - self.birthday >= 18
+  end
+
+  def can_have_cat?
+    self.name[0].downcase != 'a'
+  end
+  
+  def can_have_cat?
+    self.name[0].downcase != 'a'
+  end
+
 end
