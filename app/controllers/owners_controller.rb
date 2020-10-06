b2a78914c06173071c2bfbd690240211a5e20292
Marina Rozas <rozasmarina@gmail.com>
2020-10-06 11:23:49 -0300

    owners controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,38 @@
 class OwnersController < ApplicationController
+  before_action :set_owner, only: %i[show, destroy]
+
+  def index
+    @owners = Owner.all
+  end
+
+  def show 
+  end
+
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    @owner = Owner.new(owner_params)
+    if @owner.save
+      redirect_to owner_path(@owner)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @owner.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_owner
+    @owner = Owner.find(params[:id])
+  end
+
+  def owner_params
+    params.require(:owner).permit(:name, :birthday, :document)
+  end
 end
